03bcd8d7476f64527a9cce75010392d0038dd4c7
subu <subramanya4shenoy@gmail.com>
2022-05-11 01:00:20 +0530

    webpacks for prod build

--- marketing/config/webpack.dev.js
+++ marketing/config/webpack.dev.js
@@ -1,5 +1,4 @@
 const { merge } = require('webpack-merge');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 
@@ -21,9 +20,6 @@ const devConfig = {
                 './MarketingApp': './src/bootstrap'
             },
             shared: ['react', 'react-dom'],
-        }),
-        new HtmlWebpackPlugin({
-            template: './public/index.html'
         })
     ]
 };
